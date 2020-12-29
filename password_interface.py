63cbd1e7ff1618b7a80adae0755a7b9cb10d629d
Stevewong2007082 <40566101+Stevewong2007082@users.noreply.github.com>
2020-12-30 02:20:21 +0800

    add delete function

--- password_interface.py
+++ password_interface.py
@@ -1,5 +1,7 @@
 import sqlite3
 from sqlite3 import Error
+import random
+import string
 
 program_password_set = "123"
 web_address = ""
@@ -54,16 +56,39 @@ def check_input_valid(web, u_id, u_pass):
     global web_address
     global user_id
     global user_password
+    web_counter = 0
+    id_counter = 0
+    password_counter = 0
     while True:
         print()
-        if (web_address == "") & web:
+        if (web_address == "") & web & (("http://" not in web_address[:7]) | ("https://" not in web_address[:8])):
             web_address = input("Enter web address: ")
+            web_counter += 1
+            if web_counter > 1:
+                print("Your input must be contain 'http://' or 'https://' in the first")
         elif (user_id == "") & u_id:
             user_id = input("Enter your login id: ")
+            id_counter += 1
+            if id_counter > 1:
+                print("Your id cannot be null")
         elif (user_password == "") & u_pass:
+            print("(!random!) for random password")
             user_password = input("Enter your login password: ")
+            password_counter += 1
+            if password_counter > 1:
+                print("Your password cannot be null")
+            if "!random!" in user_password:
+                size = input("length of password")
+                if type(size) == int:
+                    user_password= random_password(size)
+                else:
+                    print("Input must be an integer")
         else:
             return True
+
+def random_password(size):
+    out = "".join(random.choice(string.ascii_letters + string.digits) for i in range(size))
+    return out
 
 def reset_val():
     global web_address
@@ -72,6 +97,20 @@ def reset_val():
     web_address = 0
     user_id = 0
     user_password = 0
+
+def find_and_print_information(web_of_database = ""):
+    count = 0
+    get_list = []
+    for info in infos:
+        if (web_of_database in info[3]) | (web_of_database == ""):
+            count += 1
+            get_list.append(info[0])
+            print("\nNumber: " + str(count))
+            print("ID: " + str(info[0]))
+            print("Web address: " + info[3])
+            print("Login id: " + info[1])
+            print("Login password: " + info[2])
+    return get_list
 
 if __name__ == '__main__':
     database = r"Password_Database.db"
@@ -98,9 +137,10 @@ if __name__ == '__main__':
             print("\nMain Menu")
             print("1. Add login information")
             print("2. Edit login information")
-            print("3. view all login information")
-            print("4. Search login information")
-            print("5. Quit")
+            print("3. Remove a login information")
+            print("4. view all login information")
+            print("5. Search login information")
+            print("6. Quit")
             choose = input("\nEnter your choose: ")
             if choose == "1":
                 if check_input_valid(True, True, True):
@@ -121,30 +161,17 @@ if __name__ == '__main__':
                 if conn is not None:
                     infos = execute_read_query(conn, view_all_login_info)
                 web_of_database = input("\nEnter the website name: ")
-                count = 0
-                get_list = []
-                for info in infos:
-                    if web_of_database in info[3]:
-                        count += 1
-                        get_list.append(info[0])
-                        print("\nNumber: " + str(count))
-                        print("ID: " + str(info[0]))
-                        print("web address: " + info[3])
-                        print("login id: " + info[1])
-                        print("login password: " + info[2])
-                if count != 1:
-                    input_number = input("\nEnter the number: ")
-                else:
-                    input_number = 1
+                get_list = find_and_print_information(web_of_database)
+                input_number = int(input("\nEnter the number: "))
                 print("\nEdit menu")
                 print("1. Edit all")
                 print("2. Edit login id")
                 print("3. Edit login password")
                 print("4. Edit website address")
                 print("5. Quit")
-                edit_number = input("\nEnter the number: ")
-                if (int(edit_number) <= 4) & (count != 0) & (int(input_number) <= len(get_list)):
-                    if edit_number == "1":
+                edit_number = int(input("\nEnter the number: "))
+                if (edit_number <= 4) & (input_number <= len(get_list)):
+                    if edit_number == 1:
                         edit_all = """
                         UPDATE id_password
                         SET login_id = ?
@@ -157,7 +184,7 @@ if __name__ == '__main__':
                         if conn is not None:
                             execute_query(conn, edit_all, edit_all_val)
                             print("Login information updated")
-                    elif edit_number == "2":
+                    elif edit_number == 2:
                         edit_login_id = """
                         UPDATE id_password
                         SET login_id = ?
@@ -168,7 +195,7 @@ if __name__ == '__main__':
                         if conn is not None:
                             execute_query(conn, edit_login_id, edit_login_id_val)
                             print("Login id updated")
-                    elif edit_number == "3":
+                    elif edit_number == 3:
                         edit_password = """
                         UPDATE id_password
                         SET login_password = ?
@@ -179,7 +206,7 @@ if __name__ == '__main__':
                         if conn is not None:
                             execute_query(conn, edit_password, edit_password_val)
                             print("Login password updated")
-                    elif edit_number == "4":
+                    elif edit_number == 4:
                         edit_web = """
                         UPDATE id_password
                         SET website_address = ?
@@ -192,33 +219,31 @@ if __name__ == '__main__':
                             print("Web address updated")
                     else:
                         print("Quit")
-
             elif choose == "3":
                 if conn is not None:
                     infos = execute_read_query(conn, view_all_login_info)
-                    i = 0
-                    for info in infos:
-                        i += 1
-                        print("\nrecord: " + str(i))
-                        print("ID: " + str(info[0]))
-                        print("web address: " + info[3])
-                        print("login id: " + info[1])
-                        print("login password: " + info[2])
+                web_of_database = input("\nEnter the website name: ")
+                get_list = find_and_print_information(web_of_database)
+                (input_number) = int(input("\nEnter the number: "))
+                if input_number < (len(get_list) + 1):
+                    remove_information = """
+                            DELETE FROM id_password
+                            WHERE id = ?;
+                            """
+                    ask = input("Do you want to remove this record? (Yes/No) ")
+                    if ("Yes" in ask) & (conn is not None):
+                        execute_query(conn, remove_information, (get_list[input_number - 1], ))
                 else:
-                    print("\nCannot view the info")
+                    print("Out of the range")
             elif choose == "4":
                 if conn is not None:
                     infos = execute_read_query(conn, view_all_login_info)
+                find_and_print_information()
+            elif choose == "5":
+                if conn is not None:
+                    infos = execute_read_query(conn, view_all_login_info)
                 web_of_database = input("\nEnter the website name: ")
-                count = 0
-                for info in infos:
-                    if web_of_database in info[3]:
-                        count += 1
-                        print("\nNumber: " + str(count))
-                        print("ID: " + str(info[0]))
-                        print("Web address: " + info[3])
-                        print("Login id: " + info[1])
-                        print("Login password: " + info[2])
+                find_and_print_information(web_of_database)
             else:
                 print("\nProcess end")
                 break
